207779b5557e5d780816c87afde33a0856c6f5b0
Krishna-Raibhat <krishnarb08@gmail.com>
2026-02-18 13:22:27 +0545

    ditributor get api

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { login, register, registerDistributor, getProfile, updatePassword, updateProfile } from "../controllers/authController";
+import { login, register, registerDistributor, getAllUsers, getProfile, updatePassword, updateProfile } from "../controllers/authController";
 import { authMiddleware } from "../middlewares/authMiddleware";
 import { roleMiddleware } from "../middlewares/roleMiddleware";
 
@@ -9,8 +9,9 @@ const router = Router();
 router.post("/register", register); // Only creates 'user' role
 router.post("/login", login);
 
-// Admin-only route to register distributors
+// Admin-only routes
 router.post("/register-distributor", authMiddleware, roleMiddleware(["admin"]), registerDistributor);
+router.get("/users", authMiddleware, roleMiddleware(["admin"]), getAllUsers); // Get all users or filter by role
 
 // Protected routes
 router.get("/profile", authMiddleware, getProfile);
